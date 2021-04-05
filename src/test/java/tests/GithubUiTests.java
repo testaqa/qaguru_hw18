32b0fdae5fed147700820da1cbf90f10b65026cd
Vasiliy Golban <vgolban@accesssoftek.com>
2021-04-05 10:57:43 +0300

    Add Selenide with UI test

--- src/test/java/tests/GithubUiTests.java
+++ src/test/java/tests/GithubUiTests.java
@@ -2,11 +2,24 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.have;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class GithubUiTests {
+
+    String issueTitle = "Test Issue Title Here";
+    String issueBody = "Some issue description";
 
     @Test
     void GithubIssueUiTest() {
+        open("https://github.com/testaqa/qaguru_hw18/issues/");
 
+        $("div[aria-label='Issues']").$(linkText(issueTitle)).click();
+
+        $(".comment-body p").should(have(text(issueBody)));
     }
 
 }
