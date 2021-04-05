7b66419542a6a4f3f3e9393605bb796ee5d77eec
Vasiliy Golban <vgolban@accesssoftek.com>
2021-04-05 12:10:51 +0300

    Add AttachmentsHelper for allure

--- src/test/java/tests/GithubUiTests.java
+++ src/test/java/tests/GithubUiTests.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.DriverHelper;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Condition.have;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static helpers.AttachmentsHelper.*;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
@@ -23,6 +25,14 @@ public class GithubUiTests {
     public static void beforeAll() {
         DriverHelper.configureDriver();
         SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true).savePageSource(true));
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        attachVideo();
     }
 
     @Test
